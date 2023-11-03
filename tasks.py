45002003b96f3aa2d1efa8d386b8997515c19904
Ankur Roy <59106018+ankurr0y@users.noreply.github.com>
2023-11-03 15:18:54 +0100

    Add files via upload

---
+++ tasks.py
@@ -0,0 +1,15 @@
+#TODO: Write a script to input text from a user and print it out
+
+#TODO: Write a script to input two values, both integers. Print their sum if they are both integers. Print "Incorrect input" of they are not.
+
+#TODO: Write a function which returns different values based on data type of input given to it.
+
+#TODO: Write a function which accepts a string (s) and an integer (x) as a value and returns a value that is that string s for x number of times.
+
+#TODO: Write a function which searches a dictionary for the key "source" and retrieves its value. Handle the exception for the error using the KeyError exception
+
+#TODO: Write a function that iterates over a string in two ways: 1. converting the string to a list and iterating over the index by calculating its length and 2. parsing the string itself with the for loop
+
+#TODO: Write a function that uses regular expressions to check whether a given string is in the email format or not.
+
+#TODO: Write a function that returns the number of words in a string separated by spaces. Check this with two pytest tests: one that uses the input "Hello World" and passes if the return value is 2 and another that fails the same case.

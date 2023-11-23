4a3ad6be87ef6a2d6a06861b67e5b9b2dc519845
Victor Wong <victor.ju.hin.wong@gmail.com>
2023-11-23 09:11:07 +0100

    Work on Assignment

--- A1_unit_testing_students/test_searchAndBuyProduct.py
+++ A1_unit_testing_students/test_searchAndBuyProduct.py
@@ -4,7 +4,7 @@ import pytest
 
 @pytest.fixture
 def login_stub(mocker):
-    return mocker.patch('products.login', return_value=None)
+    return mocker.patch('login.login', return_value='login')
 
 
 @pytest.fixture
@@ -21,6 +21,19 @@ def display_filtered_table_stub(mocker):
 def checkoutAndPayment_stub(mocker):
     return mocker.patch('checkout_and_payment.checkoutAndPayment', return_value='')
 
+
+@pytest.fixture
+def dummy_csv_file(tmp_path):
+    # Create a CSV file without a header
+    csv_filename = tmp_path / 'dummy.csv'
+    with open(csv_filename, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['Product', 'Price', 'Units'])
+        csv_writer.writerow(['Apple', 1.0, 10])
+        csv_writer.writerow(['Banana', 0.5, 20])
+        csv_writer.writerow(['Orange', 0.8, 15])
+    return csv_filename
+
 """
 def test_1_assert_login_call(login_stub, mocker):
     result = searchAndBuyProduct()
@@ -34,6 +47,7 @@ def test_2_(login_stub, display_csv_as_table_stub, display_filtered_table_stub, 
     monkeypatch.setattr("builtins.input", lambda msg: next(responses))
     login_stub.assert_called_once()
     display_csv_as_table_stub.assert_called_once()
+    checkoutAndPayment_stub.assert_called_once()
 
 """
 def test_3_(login_stub, display_csv_as_table_stub, mocker, monkeypatch):

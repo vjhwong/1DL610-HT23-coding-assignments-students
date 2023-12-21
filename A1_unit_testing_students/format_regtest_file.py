f0123ab855fae10036f20fbda1835273ca7f893a
Lucas Åhl <lucas.ahl01@gmail.com>
2023-12-21 12:38:10 +0100

    forgor

---
+++ A1_unit_testing_students/format_regtest_file.py
@@ -0,0 +1,49 @@
+def test_format_regtest():
+    # file to write to
+    write_file = "regtest_smoketest"
+
+    # resets file
+    with open(write_file, "w") as regtest:
+        regtest.write("")
+    # name of log file
+    with open('regtest_luah', "r") as file:
+        lines = file.readlines()
+        count_failed = 0
+        count_passed = 0
+        for line in lines:
+            sum = "Test called "
+            if line.__contains__("PASSED"):
+                count_passed += 1
+                splits = line.split()
+                for splitted in splits:
+                    if splitted.__contains__("name"):
+                        sum += splitted.split('\'')[1]
+                sum += " passed\n"
+                print(sum)
+                with open(write_file, "a") as regtest:
+                    regtest.write(sum)
+            elif line.__contains__("FAILED"):
+                count_failed += 1
+                splits = line.split()
+                print(splits)
+                for splitted in splits:
+                    print(splitted)
+                    if splitted.__contains__("name") and (not splitted.__contains__("|")) and (not splitted.__contains__(":")):
+                        sum += splitted.split('\'')[1]
+                sum += " failed\n"
+                print(sum)
+                with open(write_file, "a") as regtest:
+                    regtest.write(sum)
+
+        total = count_passed + count_failed
+
+        with open(write_file, "a") as regtest:
+            regtest.write("Total tests = " + str(total) + "\n")
+            regtest.write("Total passed = " + str(count_passed) + "\n")
+            regtest.write("Total failed = " + str(count_failed) + "\n")
+            regtest.write("Pass rate = " + str((count_passed / total)*100) + "%\n")
+
+        print("Total tests = " + str(total))
+        print("Total passed = " + str(count_passed))
+        print("Total failed = " + str(count_failed))
+        print("Pass rate = " + str((count_passed / total)*100) + "%")
